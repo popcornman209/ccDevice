eca32ba76c1afce27199f9dbbd486dfa14d970ab
Leo <135404395+popcornman209@users.noreply.github.com>
2024-03-10 12:51:26 -0400

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,50 +1,64 @@
-function prnt(str) if log then print(str) end end
+function prnt(str, log) if log then print(str) end end
 
-prnt("id: "..fileId)
-prnt("current version: "..currentVersion)
+function download(fileId, currentVersion, log, address, device)
+    prnt("id: "..fileId, log)
+    prnt("current version: "..currentVersion, log)
 
-settings.load("data/serverData")
-address = settings.get("address")
-device = settings.get("device")
+    if address == nil or device == nil then
+        settings.load("data/serverData")
+        address = settings.get("address")
+        device = settings.get("device")
+    end
 
-prnt("connecting...")
-ws = http.websocket(address)
+    prnt("connecting...", log)
+    ws = http.websocket(address)
 
-if ws ~= false then 
-    ws.send(os.getComputerLabel())
-    ws.send("version")
-    ws.send(device.."/"..fileId)
-    prnt("getting newest version...")
-    version = ws.receive()
-    if version ~= "goodbye" then
-        prnt("newest version: "..version)
-        if version ~= currentVersion then
-            ws.send("download")
-            ws.send(device.."/"..fileId)
-            paths = {}
-            data = {}
-            receiving = true
-            while receiving do
-                message = ws.receive()
-                if message ~= "complete" then
-                    table.insert(data, message)
-                    path = ws.receive()
-                    table.insert(paths, path)
-                else receiving = false end
+    if ws ~= false then 
+        ws.send(os.getComputerLabel())
+        ws.send("version")
+        ws.send(device.."/"..fileId)
+        prnt("getting newest version...", log)
+        version = ws.receive()
+        if version ~= "goodbye" then
+            prnt("newest version: "..version, log)
+            if version ~= currentVersion then
+                ws.send("download")
+                ws.send(device.."/"..fileId)
+                paths = {}
+                data = {}
+                receiving = true
+                while receiving do
+                    message = ws.receive()
+                    if message ~= "complete" then
+                        table.insert(data, message)
+                        path = ws.receive()
+                        table.insert(paths, path)
+                    else receiving = false end
+                end
+                prnt("installing...", log)
+
+                for i = 1,table.getn(paths) do
+                    file = fs.open(paths[i],"w")
+                    file.write(data[i])
+                    file.close()
+                    prnt("installed "..paths[i], log)
+                end
+                prnt("completed.", log)
+                ws.send("close")
+                prnt("disconnected", log)
+                return true, "success"
+            else
+                prnt("up to date.", log)
+                ws.send("close")
+                prnt("disconnected", log)
+                return false, "up to date"
             end
-            prnt("installing...")
-
-            for i = 1,table.getn(paths) do
-                file = fs.open(paths[i],"w")
-                file.write(data[i])
-                file.close()
-                prnt("installed "..paths[i])
-            end
-            prnt("completed.")
-            if restart then os.reboot() end
-        else prnt("up to date.") end
-        ws.send("close")
-        prnt("disconnected")
-    else prnt("failed: file doesnt exsist") end
-else prnt("could not connect to server")
+        else
+            prnt("failed: file doesnt exist", log)
+            return false, "files doesnt exist"
+        end
+    else
+        prnt("could not connect to server", log)
+        return false, "couldnt connect to server"
+    end
 end
