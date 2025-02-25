0d188a0cf5711e1a854b1919788c77840a7f8ee9
leo <mail@leofraser.com>
2025-02-25 14:43:16 -0500

    working on secureNet more

--- server/modules/secureNet.py
+++ server/modules/secureNet.py
@@ -4,7 +4,20 @@ if not os.path.isdir("moduleFiles/secureNet"): os.mkdir("moduleFiles/secureNet")
 
 activeDnsConnections = {}
 
-def registerDns(hostName):
+class connection:
+    def __init__(self, websocket, key, recieveBroadcasts, listeningChannels):
+        self.ws = websocket
+        self.key = key
+        self.rcvBroad = recieveBroadcasts
+        self.channel = listeningChannels
+    
+    async def sendMsg(self,message,channel,broadcast):
+        if self.ws.connected and (channel in self.channels or self.channels == []) and (self.rcvBroad or broadcast == False):
+            await self.ws.send(message)
+            return True
+        else: return False
+
+def registerStaticDNS(hostName):
     if hostName not in os.listdir("moduleFiles/secureNet"):
         key = str(random.randint(0,standard.settings["secureNetKeyLength"]))
         with open("moduleFiles/secureNet/"+hostName,"w") as f:
@@ -12,14 +25,20 @@ def registerDns(hostName):
         return key
     else: return False
 
-def connectDns(hostName, key, websocket):
+def connectStaticDNS(hostName, key, websocket, recieveBroadcasts = True, listeningChannelss = []):
     if hostName in os.listdir("moduleFiles/secureNet"):
         with open("moduleFiles/secureNet/"+hostName,"r") as f:
             if f.read() != key: return False
         global activeDnsConnections
-        activeDnsConnections[hostName] = websocket
+        activeDnsConnections[hostName] = connection(websocket, recieveBroadcasts, listeningChannelss)
         return True
     else: return False
+
+def modifyDNS(hostName, key, channels = None, rcvBroad = None):
+    if hostName in activeDnsConnections:
+        if activeDnsConnections[hostName].key == key:
+            if channels: activeDnsConnections[hostName].channels = channels
+            if rcvBroad: activeDnsConnections[hostName].rcvBroad = rcvBroad
 
 apiCalls = {
 }
