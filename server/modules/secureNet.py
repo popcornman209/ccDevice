452dd574baea24f752fe30bd2ec5e78be7fe9923
leo <mail@leofraser.com>
2025-02-26 12:06:37 -0500

    secureNet commenting

--- server/modules/secureNet.py
+++ server/modules/secureNet.py
@@ -6,76 +6,81 @@ activeDnsConnections = {}
 
 class connection:
     def __init__(self, websocket, hostName, key, recieveBroadcasts, listeningChannels):
-        self.ws = websocket
-        self.hName = hostName
-        self.key = key
-        self.rcvBroad = recieveBroadcasts
-        self.channel = listeningChannels
+        self.ws = websocket #websocket connection, what to send messages to.
+        self.hName = hostName #hostname, can be used to index activeDnsConnections
+        self.key = key #private key, used for authenticating that the sender is who they say they are
+        self.rcvBroad = recieveBroadcasts #wehter to receive broadcasts or not, spam
+        self.channel = listeningChannels #wwhat channels to listen to
     
-    async def rcvMsg(self,sender,message,channel,broadcast):
-        if self.ws.connected and (channel in self.channels or self.channels == []) and (self.rcvBroad or broadcast == False):
-            await self.ws.send(json.dumps({
+    async def rcvMsg(self,sender,message,channel,broadcast): #recieve messages
+        if self.ws.connected and channel in self.channels and (self.rcvBroad or broadcast == False): #if should be recieved, if connected, listening to provided channel, and if its broadcasted wether listening to that
+            await self.ws.send(json.dumps({ #send the message
                 "sender": sender.hName,
                 "message": message,
                 "channel": channel,
                 "broadcasted": broadcast
             }))
-            return True
-        else: return False
+            return True #success
+        else: return False #failure
     
     async def sendMsg(self,message,reciever,channel=""):
-        if type(reciever) == str: rcv = activeDnsConnections[reciever]
-        else: rcv = reciever
-        return await rcv.rcvMsg(self,message,channel,False)
+        if type(reciever) == str: rcv = activeDnsConnections[reciever] #if hostname provided
+        else: rcv = reciever #if connection object provided
+        return await rcv.rcvMsg(self,message,channel,False) #send message
     
-    async def broadcast(self,message,channel=""):
-        for connection in activeDnsConnections:
-            await connection.rcvMsg(self,message,channel,True)
-        return True
+    async def broadcast(self,message,channel=""): #broadcast message
+        for connection in activeDnsConnections: #go through all connections
+            await connection.rcvMsg(self,message,channel,True) #broadcast a message to them
+        return True #success
 
-class serverConnection(connection):
-    def __init__(self, hostName, key):
-        self.ws = None
-        self.hName = hostName
-        self.key = key
-        self.rcvBroad = False
-        self.channels = None
-    def rcvMsg(self, sender, message, channel, broadcast): return False
+class serverConnection(connection): #fake connection, cant recieve messages
+    def __init__(self, hostName, key, messageRecievedMethod=None):
+        super().__init__(None,hostName,key,False,[""]) #initiate
+        self.rcvMethod = messageRecievedMethod #sets the recieving message method
 
-def verifyDNS(hostName, key):
-    if hostName in activeDnsConnections:
-        if activeDnsConnections[hostName].key == key:
-            return True
-    return False
+    def rcvMsg(self,sender,message,channel,broadcast): #recieve message
+        if self.rcvMethod != None: #if able to recieve messages
+            self.rcvMethod(sender,message,channel,broadcast) #do so
+            return True #sucess
+        else: return False #failure
 
-def registerStaticDNS(hostName):
-    if hostName not in os.listdir("moduleFiles/secureNet"):
-        key = str(random.randint(0,standard.settings["secureNetKeyLength"]))
-        with open("moduleFiles/secureNet/"+hostName,"w") as f:
-            f.write(key)
-        return key
-    else: return False
 
-def registerDynamicDNS(hostName, websocket, recieveBroadcasts = True, listeningChannelss = []):
-    key = str(random.randint(0,standard.settings["secureNetKeyLength"]))
-    activeDnsConnections[hostName] = serverConnection(websocket,hostName,key,recieveBroadcasts,listeningChannelss)
-    return key
+def verifyDNS(hostName, key): #authenticate connection
+    if hostName in activeDnsConnections: #if connection exists
+        if activeDnsConnections[hostName].key == key: #if key is valid
+            return True #return success
+    return False #failure
 
-def connectStaticDNS(hostName, key, websocket, recieveBroadcasts = True, listeningChannelss = []):
-    if hostName in os.listdir("moduleFiles/secureNet"):
-        with open("moduleFiles/secureNet/"+hostName,"r") as f:
-            if f.read() != key: return False
+def registerStaticDNS(hostName): #register a static dns address
+    if hostName not in os.listdir("moduleFiles/secureNet"): #if not already taken
+        key = standard.randString(standard.settings["secureNetKeyLength"]) #generate a key
+        with open("moduleFiles/secureNet/"+hostName,"w") as f: #save the data
+            f.write(key) #write the key
+        return key #return the key
+    else: return False #failure
+
+def connectTempDNS(hostName, websocket, recieveBroadcasts = True, listeningChannelss = []): #register and connect temporary adress
+    key = standard.randString(standard.settings["secureNetKeyLength"]) #gen a key
+    activeDnsConnections[hostName] = serverConnection(websocket,hostName,key,recieveBroadcasts,listeningChannelss) #connect
+    return key #return key
+
+def connectStaticDNS(hostName, key, websocket, recieveBroadcasts = True, listeningChannelss = []): #connect to a static dns address
+    if hostName in os.listdir("moduleFiles/secureNet"): #if exists
+        with open("moduleFiles/secureNet/"+hostName,"r") as f: #open file
+            if f.read() != key: return False #if invalid cancel
         global activeDnsConnections
-        activeDnsConnections[hostName] = connection(websocket, hostName, key, recieveBroadcasts, listeningChannelss)
-        return True
-    else: return False
+        activeDnsConnections[hostName] = connection(websocket, hostName, key, recieveBroadcasts, listeningChannelss) #connect the address
+        return True #success
+    else: return False #failure
 
-def shutdownDNS(hostName,key):
-    if verifyDNS(hostName,key):
+def diconnectDNS(hostName,key): #disconnect dns adress
+    if verifyDNS(hostName,key): #if valid
         global activeDnsConnections
-        del(activeDnsConnections[hostName])
-        return True
-    else: return False
+        del(activeDnsConnections[hostName]) #disconnect the adress
+        return True #success
+    else: return False #failure
+
+
 
 apiCalls = {
 }
